fd7a179795eacd1982fd26c1bf5e03fc66ab5bc4
hamasfaa <hamasfa@gmail.com>
2025-03-14 18:35:03 +0700

    Update color thresholds for Lapangan and Line detection; optimize contour merging logic

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 using namespace cv;
 
 // Lapangan
-int hminLapangan = 66, sminLapangan = 123, vminLapangan = 146;
-int hmaxLapangan = 134, smaxLapangan = 255, vmaxLapangan = 255;
+int hminLapangan = 70, sminLapangan = 186, vminLapangan = 146;
+int hmaxLapangan = 122, smaxLapangan = 255, vmaxLapangan = 255;
 
 // Team A
 int hminTeamA = 56, sminTeamA = 13, vminTeamA = 73;
@@ -17,8 +17,8 @@ int hminBall = 0, sminBall = 162, vminBall = 125;
 int hmaxBall = 51, smaxBall = 255, vmaxBall = 255;
 
 // Line
-int hminLine = 0, sminLine = 0, vminLine = 0;
-int hmaxLine = 179, smaxLine = 255, vmaxLine = 255;
+int hminLine = 0, sminLine = 149, vminLine = 0;
+int hmaxLine = 255, smaxLine = 255, vmaxLine = 255;
 
 static const double thresholdAreaTimA = 150.0;
 static const double thresholdAreaBall = 150.0;
@@ -30,13 +30,13 @@ int main(int, char **)
     Mat frame, frameHSV, frameBlur, frameHSL;
     Mat maskLapangan, maskTeamA, kernel, maskBall, maskLine;
 
-    namedWindow("TrackbarsLine", (640, 200));
-    createTrackbar("Hue Min", "TrackbarsLine", &hminLine, 255);
-    createTrackbar("Hue Max", "TrackbarsLine", &hmaxLine, 255);
-    createTrackbar("Sat Min", "TrackbarsLine", &sminLine, 255);
-    createTrackbar("Sat Max", "TrackbarsLine", &smaxLine, 255);
-    createTrackbar("Val Min", "TrackbarsLine", &vminLine, 255);
-    createTrackbar("Val Max", "TrackbarsLine", &vmaxLine, 255);
+    // namedWindow("TrackbarsLapangan", (640, 200));
+    // createTrackbar("Hue Min", "TrackbarsLapangan", &hminLapangan, 255);
+    // createTrackbar("Hue Max", "TrackbarsLapangan", &hmaxLapangan, 255);
+    // createTrackbar("Sat Min", "TrackbarsLapangan", &sminLapangan, 255);
+    // createTrackbar("Sat Max", "TrackbarsLapangan", &smaxLapangan, 255);
+    // createTrackbar("Val Min", "TrackbarsLapangan", &vminLapangan, 255);
+    // createTrackbar("Val Max", "TrackbarsLapangan", &vmaxLapangan, 255);
 
     while (true)
     {
@@ -59,97 +59,45 @@ int main(int, char **)
         kernel = getStructuringElement(MORPH_ELLIPSE, Size(20, 20));
         morphologyEx(maskLapangan, maskLapangan, MORPH_CLOSE, kernel);
 
-        vector<vector<Point>> contoursLapangan, contoursBall, contoursTeamA;
-        vector<Vec4i> hierarchyLapangan, hierarchyBall, hierarchyTeamA;
+        vector<vector<Point>> contoursLapangan, contoursBall, contoursTeamA, contoursLine;
+        vector<Vec4i> hierarchyLapangan, hierarchyBall, hierarchyTeamA, hierarchyLine;
         findContours(maskLapangan, contoursLapangan, hierarchyLapangan, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
         findContours(maskTeamA, contoursTeamA, hierarchyTeamA, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
         findContours(maskBall, contoursBall, hierarchyBall, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
-        int largestIndex = -1;
-        double maxArea = 0.0;
+        vector<Rect> boundingRects;
         for (size_t i = 0; i < contoursLapangan.size(); i++)
         {
             double area = contourArea(contoursLapangan[i]);
-            if (area > maxArea)
+            if (area > thresholdAreaTimA)
             {
-                maxArea = area;
-                largestIndex = static_cast<int>(i);
-            }
-        }
+                Rect boundingRectLapangan = boundingRect(contoursLapangan[i]);
+                bool merged = false;
 
-        Mat mergedLapangan = Mat::zeros(maskLapangan.size(), CV_8UC1);
-        if (largestIndex != -1)
-        {
-            drawContours(mergedLapangan, contoursLapangan, largestIndex, Scalar(255), -1);
-        }
-
-        Rect rectLap;
-        if (largestIndex != -1)
-        {
-            rectLap = boundingRect(contoursLapangan[largestIndex]);
-            rectangle(frame, rectLap, Scalar(0, 255, 0), 2);
-        }
-
-        for (size_t j = 0; j < contoursTeamA.size(); j++)
-        {
-            double areaTeamA = contourArea(contoursTeamA[j]);
-            if (areaTeamA < thresholdAreaTimA)
-            {
-                continue;
-            }
-
-            Rect bboxTeamA = boundingRect(contoursTeamA[j]);
-            bool isInsideLapangan = false;
-
-            if (largestIndex != -1)
-            {
-                Rect overlap = rectLap & bboxTeamA;
-                if (overlap.area() > 0)
+                for (auto &rect : boundingRects)
                 {
-                    isInsideLapangan = true;
+                    if ((abs(boundingRectLapangan.x - rect.x) < 1000) && (abs(boundingRectLapangan.y - rect.y) < 1000))
+                    {
+                        rect |= boundingRectLapangan;
+                        merged = true;
+                        break;
+                    }
+                }
+
+                if (!merged)
+                {
+                    boundingRects.push_back(boundingRectLapangan);
                 }
             }
-
-            if (isInsideLapangan)
-            {
-                drawContours(frame, contoursTeamA, (int)j, Scalar(0, 0, 255), 2);
-            }
         }
 
-        for (size_t k = 0; k < contoursBall.size(); k++)
+        for (const auto &rect : boundingRects)
         {
-            double areaBall = contourArea(contoursBall[k]);
-            if (areaBall < thresholdAreaBall)
-            {
-                continue;
-            }
-
-            Point2f center;
-            float radius;
-            minEnclosingCircle(contoursBall[k], center, radius);
-            bool isBallInsideLapangan = false;
-
-            if (largestIndex != -1)
-            {
-                if (rectLap.contains(center))
-                {
-                    isBallInsideLapangan = true;
-                }
-            }
-
-            circle(frame, center, (int)radius, Scalar(0, 255, 0), 2);
-            if (isBallInsideLapangan)
-            {
-                putText(frame, "Bola di dalam lapangan", Point(10, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
-            }
-            else
-            {
-                putText(frame, "Bola di luar lapangan", Point(10, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
-            }
+            rectangle(frame, rect, Scalar(0, 255, 0), 2);
         }
 
         imshow("Frame", frame);
-        imshow("Mask Lapangan", maskLine);
+        imshow("Mask Lapangan", maskLapangan);
 
         if (waitKey(18) == 27)
             break;
